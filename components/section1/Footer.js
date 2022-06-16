00e4ed09cd5e7683bece106db90bd24dfa519fe3
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-06-16 09:53:05 -0700

    push

--- components/section1/Footer.js
+++ components/section1/Footer.js
@@ -1,16 +1,55 @@
 import React from "react";
+import CallButton from "./CallButton"
 
 export default function Footer() {
   return (
     <div className="flex flex-col items-center">
+      <div className="w-full bg-center bg-cover bg-gray-50 py-10 text-center flex items-center justify-center">
+        <div className="w-10/12 py-10 md:w-8/12">
+          <h1 className="text-3xl font-bold">Related Services</h1>
+          <ul className="mt-4 columns-3">
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+          </ul>
+          <br /><br />
+          <CallButton phoneText="+1-810-594-5229" phoneNo="+18105945229" />
+        </div>
+      </div>
       <div className="w-full bg-center bg-cover bg-cta py-20 text-center flex items-center justify-center">
         <div className="bg-white w-10/12 py-10 rounded-3xl md:w-8/12">
           <h1 className="text-3xl font-bold">Related Tags</h1>
-          <ul className="mt-4">
-            <li>Test Alturas, CA</li>
-            <li>Test Alturas, CA</li>
-            <li>Test Alturas, CA</li>
+          <ul className="mt-4 columns-3">
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
+            <li className="mt-2">Test Alturas, CA</li>
           </ul>
+        </div>
+      </div>
+      <div className="w-full py-20 bg-black flex justify-center text-white">
+        <div className="w-10/12 grid grid-cols-3 gap-10">
+          <div>
+            <h1 className="text-4xl font-bold">5 Star Review!</h1>
+            <img src="/images/icons/starY.png" className="my-5" alt="" />
+            <p>
+              Everyday Locksmith provides a 24/7 locksmith services. We are
+              licensed and insured, and all of our locksmiths are up to date
+              with the latest security technology.
+            </p>
+          </div>
+          <div>
+            <h1 className="text-3xl capitalize">We accept all Major credit Cards!</h1>
+            <img src="/images/pay-cards.png" className="w-full mt-5" alt="" />
+          </div>
+          <div>
+          <CallButton phoneText="+1-810-594-5229" phoneNo="+18105945229" />
+          </div>
         </div>
       </div>
     </div>
--- components/section1/Header.js
+++ components/section1/Header.js
@@ -4,17 +4,16 @@ import Link from "next/link";
 import MenuItems from "./MenuItems";
 import logo from "../../public/images/logo.png";
 import Image from "next/image";
-import ServicesMenu from './ServicesMenu'
+import ServicesMenu from "./ServicesMenu";
 
 const Header = () => {
-
   const [active, setActive] = useState(false);
   const showMenu = () => {
     setActive(!active);
   };
 
   return (
-    <div className="fixed flex z-50 items-center justify-between w-full text-gray-100 bg-black/10 md:backdrop-blur-md">
+    <div className="fixed flex z-50 items-center justify-between w-full text-gray-100 bg-black/30 md:backdrop-blur-md">
       <Image src={logo} alt="Picture of the author" width={180} height={40} />
       <nav>
         <div className="absolute scale-150 right-6 md:hidden top-6">
@@ -29,17 +28,15 @@ const Header = () => {
             <Link href="/">
               <a className="hover:underline decoration-4">Home</a>
             </Link>
-            
           </li>
           <li className="hover:text-white/90">
-          <ServicesMenu />
+            <ServicesMenu />
           </li>
           <li className="hover:text-white/90">
             <Link href="/">
               <a className="hover:underline decoration-4">About</a>
             </Link>
           </li>
-
         </ul>
 
         <MenuItems showMenu={showMenu} active={active} />
