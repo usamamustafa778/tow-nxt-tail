bde0fc99d9199537431e5f61a2522d138f5f87ad
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-06-20 00:55:53 -0700

    push

--- pages/towing/[state]/[county]/[city]/index.js
+++ pages/towing/[state]/[county]/[city]/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Zips from "../../../../../components/section1/Zips";
 
 export default function Home() {
-
+  
     const router = useRouter();
     let { county } = router.query;
   
